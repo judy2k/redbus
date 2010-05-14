93d454eb1f6c38e7687e1bef0680679aa5a45d14
adqadq <adqadq@047a1783-e752-e205-4d80-9245024c4953>
2010-05-14 20:09:51 +0000

    remove a warning
    git-svn-id: http://redbus.googlecode.com/svn/trunk@44 047a1783-e752-e205-4d80-9245024c4953

--- src/org/redbus/StopBookmarksActivity.java
+++ src/org/redbus/StopBookmarksActivity.java
@@ -35,7 +35,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class StopBookmarksActivity extends ListActivity 
 {	
