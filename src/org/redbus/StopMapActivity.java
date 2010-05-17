8934716fb7f21ab3ee9e0d535aec3c2ac1f170b6
cozzarp <cozzarp@047a1783-e752-e205-4d80-9245024c4953>
2010-05-17 15:30:17 +0000

    First pass at adding stop locations + service numbers to map
    source formatting fix
    
    
    
    git-svn-id: http://redbus.googlecode.com/svn/trunk@105 047a1783-e752-e205-4d80-9245024c4953

--- src/org/redbus/StopMapActivity.java
+++ src/org/redbus/StopMapActivity.java
@@ -18,15 +18,13 @@
 
 package org.redbus;
 
-
 import java.util.ArrayList;
-
 import org.redbus.PointTree.BusStopTreeNode;
-
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.os.Bundle;
-import android.util.Log;
-
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
@@ -40,95 +38,128 @@ public class StopMapActivity extends MapActivity {
 	public class StopOverlay extends Overlay {
 		private Projection projection;
 		private PointTree busStopLocations;
+		private Paint brush;
+		private ArrayList<BusStopTreeNode> nodes;
+		private boolean showServiceLabels;
 
 		private double tlx, oldtlx;
 		private double tly, oldtly;
 		private double brx, oldbrx;
 		private double bry, oldbry;
-		
+
 		public StopOverlay(MapView view) {
 			this.busStopLocations = PointTree.getPointTree(StopMapActivity.this);			
-	        this.projection = view.getProjection();
-	        oldtlx = oldtly = oldbrx = oldbry = 0;
-	    }
-		
+			this.projection = view.getProjection();
+			this.brush = new Paint();
+			brush.setARGB(250, 250, 0, 0); // rEdB[r]us[h] ;-)
+			brush.setAntiAlias(true);
+
+			oldtlx = oldtly = oldbrx = oldbry = 0;
+		}
+
+		private String getServicesForStop(PointTree.BusStopTreeNode node) {
+			ArrayList<String> services = busStopLocations.lookupServices(node.getServicesMap());
+
+			// Where is string.join()?
+			StringBuilder sb = new StringBuilder();
+			for(String s: services) {
+				sb.append(s);
+				sb.append(" ");
+			}
+			return sb.toString();
+		}
+
 		public void draw(Canvas canvas, MapView view, boolean shadow) {
-	        super.draw(canvas, view,shadow);
-	        
-	        GeoPoint tl = projection.fromPixels(0,canvas.getHeight());
-	        GeoPoint br = projection.fromPixels(canvas.getWidth(),0);
-	        
-	        tlx = tl.getLatitudeE6() / 1E6;
-	        tly = tl.getLongitudeE6() / 1E6;
-	        brx = br.getLatitudeE6() / 1E6;
-	        bry = br.getLongitudeE6() / 1E6;
-	        
-	        
-	        // For some reason, draw is called LOTS of times. Only requery the DB if
-	        // the co-ords change.
-	        if (tlx != oldtlx || tly != oldtly || brx != oldbrx || bry != oldbry)
-	        {
-	        	oldtlx = tlx; oldtly = tly; oldbrx = brx; oldbry = bry;
-	            Log.println(Log.DEBUG, "colin"," newpos "+tl.toString()+ " -> "+br.toString());
-		         
-	            ArrayList<BusStopTreeNode> nodes = busStopLocations.findRect(
-	        		tlx,tly,brx,bry);
-	        
-	            for (BusStopTreeNode node: nodes)
-	            {
-	        	    Log.println(Log.DEBUG, "colin", "stop "+node.getStopName());
-	            }
-	        }
-	        
+			super.draw(canvas, view,shadow);
+
+			GeoPoint tl = projection.fromPixels(0,canvas.getHeight());
+			GeoPoint br = projection.fromPixels(canvas.getWidth(),0);
+
+			tlx = tl.getLatitudeE6() / 1E6;
+			tly = tl.getLongitudeE6() / 1E6;
+			brx = br.getLatitudeE6() / 1E6;
+			bry = br.getLongitudeE6() / 1E6;
+
+			// For some reason, draw is called LOTS of times. Only requery the DB if
+			// the co-ords change.
+			if (tlx != oldtlx || tly != oldtly || brx != oldbrx || bry != oldbry) {
+				oldtlx = tlx; oldtly = tly; oldbrx = brx; oldbry = bry;
+				//Log.println(Log.DEBUG, "colin"," newpos "+tl.toString()+ " -> "+br.toString());
+
+				nodes = busStopLocations.findRect(tlx,tly,brx,bry);
+
+				// Prevent zoomed out view looking like abstract art
+				// with too many labels drawn...
+				showServiceLabels = nodes.size() < 20;
+			}
+
+			if (shadow == true)
+				return;
+
+			// For each node, draw a circle and optionally service number list
+			for (BusStopTreeNode node: nodes) {
+				int radius = 5;
+
+				double lat = node.getX() * 1E6;
+				double lng = node.getY() * 1E6;
+				Point stopCircle = new Point();
+				projection.toPixels(new GeoPoint((int)lat,(int)lng), stopCircle);
+
+				canvas.drawOval(new RectF(stopCircle.x-radius, stopCircle.y-radius,
+						stopCircle.x+radius, stopCircle.y+radius), brush);
+
+				if (showServiceLabels)
+					canvas.drawText(getServicesForStop(node), stopCircle.x+radius, stopCircle.y+radius, brush);
+			}  
 		}
 	}
-	
+
 	private MapView mapView;
 	private MapController mapController;
 	private MyLocationOverlay myLocationOverlay;
-	
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) { 
-       super.onCreate(savedInstanceState); 
-       setContentView(R.layout.stop_map);
-       
-       mapView = (MapView) findViewById(R.id.mapview);
-       mapView.setBuiltInZoomControls(true);
-       mapController = mapView.getController();
-       
-       mapController.setZoom(17);
-       
-       // Make map update automatically as user moves around
-       myLocationOverlay = new MyLocationOverlay(this, mapView);
-       
-       myLocationOverlay.runOnFirstFix(new Runnable() {
-           public void run() {
-               mapController.animateTo(myLocationOverlay.getMyLocation());
-           }
-       });
-          
-       mapView.getOverlays().add(myLocationOverlay);
-       myLocationOverlay.enableCompass();
-       
-       mapView.getOverlays().add(new StopOverlay(mapView));
+	public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); 
+		setContentView(R.layout.stop_map);
+
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mapController = mapView.getController();
+
+		mapController.setZoom(17);
+
+		// Make map update automatically as user moves around
+		myLocationOverlay = new MyLocationOverlay(this, mapView);
+
+		myLocationOverlay.runOnFirstFix(new Runnable() {
+			public void run() {
+				mapController.animateTo(myLocationOverlay.getMyLocation());
+			}
+		});
+
+		mapView.getOverlays().add(myLocationOverlay);
+		myLocationOverlay.enableCompass();
+
+		mapView.getOverlays().add(new StopOverlay(mapView));
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-	
+
 	@Override
 	public void onPause() {
-	       myLocationOverlay.disableMyLocation();
-	       super.onPause();
+		myLocationOverlay.disableMyLocation();
+		super.onPause();
 	}
-	
+
 	@Override
 	public void onResume() {
-	       myLocationOverlay.enableMyLocation();
-	       super.onResume();
+		myLocationOverlay.enableMyLocation();
+		super.onResume();
 	}
-	
-	
+
+
 }
