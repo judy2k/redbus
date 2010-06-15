43caabc0c6243e65e46908a6d8983a420bdbf506
adqadq <adqadq@047a1783-e752-e205-4d80-9245024c4953>
2010-06-15 23:34:45 +0000

    Tweak scrits
    git-svn-id: http://redbus.googlecode.com/svn/trunk@282 047a1783-e752-e205-4d80-9245024c4953

--- stopdb/autopublish.sh
+++ stopdb/autopublish.sh
@@ -22,20 +22,5 @@ INSTDIR=/usr/local/redbus
 # $INSTDIR/getstops.py || exit 1
 
 # Generate new stops database
-$INSTDIR/makestopsdat.py $INSTDIR/bus1.dat || exit 1
-newsum=`/usr/bin/md5sum $INSTDIR/bus1.dat | /bin/cut -f1 -d ' '` || exit 1
-
-# Deal with old database
-if [ ! -f $INSTDIR/stopdata/bus1.dat ]; then
-	echo "Old data file was missing!"
-	exit 1
-fi
-
-# Get the sum of the old file and copy ourselves for next time
-oldsum=`/usr/bin/md5sum $INSTDIR/stopdata/bus1.dat | /bin/cut -f1 -d ' '` || exit 1
-
-# Publish if something has changed!
-if [ x$oldsum != x$newsum ]; then
-	cp -f $INSTDIR/bus1.dat $INSTDIR/stopdata/bus1.dat
-	svn commit $INSTDIR/stopdata
-fi
+$INSTDIR/makestopsdat.py $INSTDIR/stopdata/bus1.dat || exit 1
+svn commit $INSTDIR/stopdata
