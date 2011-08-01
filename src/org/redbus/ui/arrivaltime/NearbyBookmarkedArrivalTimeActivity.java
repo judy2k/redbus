ce2c5e40653119d35ee2ae8825552bbbd2979464
cozzarp@gmail.com <cozzarp@gmail.com@047a1783-e752-e205-4d80-9245024c4953>
2011-08-01 14:25:27 +0000

    Displays all services at nearby stops. Still needs error information added
    git-svn-id: http://redbus.googlecode.com/svn/trunk@414 047a1783-e752-e205-4d80-9245024c4953

--- src/org/redbus/ui/arrivaltime/NearbyBookmarkedArrivalTimeActivity.java
+++ src/org/redbus/ui/arrivaltime/NearbyBookmarkedArrivalTimeActivity.java
@@ -19,31 +19,52 @@
 package org.redbus.ui.arrivaltime;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
+import org.redbus.R;
+import org.redbus.arrivaltime.ArrivalTime;
+import org.redbus.arrivaltime.ArrivalTimeHelper;
+import org.redbus.arrivaltime.IArrivalTimeResponseListener;
 import org.redbus.settings.SettingsHelper;
 import org.redbus.stopdb.StopDbHelper;
+import org.redbus.ui.BusyDialog;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
-public class NearbyBookmarkedArrivalTimeActivity extends Activity {
+public class NearbyBookmarkedArrivalTimeActivity extends ListActivity implements IArrivalTimeResponseListener, OnCancelListener {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
     private ArrayList<Integer> bookmarkIds; // Stop codes of current bookmarks
-    
+    private List<ArrivalTime> times; // The actual retrieved times
+    private Iterator<Integer> nextStopToDownload;
+	private BusyDialog busyDialog = null;
+	private int expectedRequestId = -1;
+	private ArrivalTimeArrayAdapter lvAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
     	super.onCreate(savedInstanceState);
     	setTitle("Nearby");
+    	busyDialog = new BusyDialog(this);
+    	this.times = new ArrayList<ArrivalTime>();
+		lvAdapter = new ArrivalTimeArrayAdapter(this, R.layout.bustimes_item, this.times);
+		setListAdapter(lvAdapter);
     }
     
     @Override
@@ -51,14 +72,29 @@ public class NearbyBookmarkedArrivalTimeActivity extends Activity {
     {
             super.onResume();
             this.bookmarkIds = getBookmarks();
+            this.times.clear();
+            this.lvAdapter.notifyDataSetChanged();
             startLocationListener();
+            busyDialog.show(this, "Finding bookmarked stops nearby");
     }
     
     @Override
     public void onPause() 
     {
         super.onPause();
-    	locationManager.removeUpdates(locationListener);
+        stopLocationListener();
+    }
+    
+    @Override
+    public void onDestroy()
+    {
+    	super.onDestroy();
+    	busyDialog.dismiss();
+    }
+    
+    private void stopLocationListener()
+    {
+    	locationManager.removeUpdates(locationListener);	
     }
     
     private void startLocationListener()
@@ -104,26 +140,86 @@ public class NearbyBookmarkedArrivalTimeActivity extends Activity {
     
     private void locationUpdated(Location location)
     {
-            // Now get the ones near us
-            int x = (int)(location.getLatitude() * 1E6);
-            int y = (int)(location.getLongitude() * 1E6);
-            
-            StopDbHelper pt = StopDbHelper.Load(this);
-            
-            ArrayList<Integer> nearby = pt.getStopsWithinRadius((int)x, (int)y, bookmarkIds, 0.008);
-            
-            // Just put in a debug string for now
-            String toastTemp = "Nearby:";
-            for(Integer stop : nearby)
-            {
-            	String name = pt.lookupStopNameByStopNodeIdx(pt.lookupStopNodeIdxByStopCode(stop));
-            	toastTemp = toastTemp + "\n"+name;
-            }
-            
-            Toast.makeText(getBaseContext(), 
-                    toastTemp, 
-                    Toast.LENGTH_SHORT).show(); 
-            
+    	stopLocationListener(); // For now only update once
+
+    	// Now get the ones near us
+    	int x = (int)(location.getLatitude() * 1E6);
+    	int y = (int)(location.getLongitude() * 1E6);
+
+    	StopDbHelper pt = StopDbHelper.Load(this);
+
+    	// 0.008 empirically determined for now!
+    	ArrayList<Integer> nearby = pt.getStopsWithinRadius((int)x, (int)y, bookmarkIds, 0.008);
+
+    	// Nearby names toast
+    	String toastTxt = "Nearby:";
+    	for(Integer stopCode : nearby)
+    	{
+    		String name = pt.lookupStopNameByStopNodeIdx(pt.lookupStopNodeIdxByStopCode(stopCode));
+    		toastTxt += "\n"+name;
+    	}
+
+    	Toast.makeText(getBaseContext(), 
+    			toastTxt, 
+    			Toast.LENGTH_SHORT).show(); 
+
+    	// Download the first stop
+    	nextStopToDownload = nearby.iterator();
+    	triggerDownloadNextStop();
     }
+
+    private void triggerDownloadNextStop()
+    {
+    	if (nextStopToDownload.hasNext())
+    	{
+    		Integer nextStop = nextStopToDownload.next();
+
+    		busyDialog.dismiss();
+    		busyDialog.show(this, "Downloading "+nextStop);
+    		
+    		expectedRequestId = ArrivalTimeHelper.getBusTimesAsync(nextStop, 0, null, NearbyBookmarkedArrivalTimeActivity.this);
+    	}
+    }
+
+    // FIXME - this is common with ArrivalTimeActivity
+	public void getBusTimesError(int requestId, int code, String message) {
+		if (requestId != expectedRequestId)
+			return;
+		
+		busyDialog.dismiss();
+
+		setListAdapter(new ArrivalTimeArrayAdapter(this, R.layout.bustimes_item, new ArrayList<ArrivalTime>()));
+		findViewById(R.id.bustimes_error).setVisibility(View.VISIBLE);
+
+		new AlertDialog.Builder(this).setTitle("Error").
+			setMessage("Unable to download bus times: " + message).
+			setPositiveButton(android.R.string.ok, null).
+			show();
+	}
+
+	public void getBusTimesSuccess(int requestId, List<ArrivalTime> busTimes) {
+		if (requestId != expectedRequestId)
+			return;
+		this.times.addAll(busTimes);
+		triggerDownloadNextStop();
+		busyDialog.dismiss();
+
+		// FIXME - this is common with ArrivalTimeActivity
+		Collections.sort(this.times, new Comparator<ArrivalTime>() {
+			public int compare(ArrivalTime arg0, ArrivalTime arg1) {
+				if ((arg0.arrivalAbsoluteTime != null) && (arg1.arrivalAbsoluteTime != null)) {
+					// bus data never seems to span to the next day, so this string comparison should always work
+					return arg0.arrivalAbsoluteTime.compareTo(arg1.arrivalAbsoluteTime);
+				}
+				return arg0.arrivalSortingIndex - arg1.arrivalSortingIndex;
+			}
+		});
+		
+		lvAdapter.notifyDataSetChanged();
+	}
+
+	public void onCancel(DialogInterface arg0) {
+		expectedRequestId = -1;
+	}
 
 }
